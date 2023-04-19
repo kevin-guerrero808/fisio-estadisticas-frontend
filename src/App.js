edd50c22fe8fcf140a03ad9c3da663fc2bef8e87
kevin-guerrero808 <35382231+kevin-guerrero808@users.noreply.github.com>
2023-04-18 23:40:02 -0500

    create proyect structure - add layout and routes (#1)
    create proyect and add layout and routes
    
    add admin pages
    
    se agregan las rutas
    
    add routes
    
    complete menu
    
    Co-authored-by: Geovani Ladino <gladino@heinsohn.com.co>

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import allRoutesProject from './config/routes'
+
+import './App.scss'
+
+const App = () => {
+  const color = {
+    primaryColor: '#0069A3',
+    errorColor: '#ff4d4f',
+    warningColor: '#faad14',
+    successColor: '#52c41a',
+    infoColor: '#1890ff',
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        { allRoutesProject.map((route, i) => (
+          <Route key={route.path} path={route.path} element={<route.layout><route.component /></route.layout>} />
+        )) }
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/config/routes.js
@@ -0,0 +1,44 @@
+import { GeneralLayout } from "../Layout/GeneralLayout/GeneralLayout";
+// import { SignIn } from "../pages/admin/SignIn";
+// import { Contact } from "../pages/Contact";
+// import { Home } from "../pages/Home";
+// import { NotFound } from "../pages/NotFound/NotFound";
+import Indicators from "../pages/Admin/Indicators/Indicators"
+import CreateUser from "../pages/Admin/Users/CreateUser/CreateUser"
+import ListUsers from "../pages/Admin/Users/ListUsers/ListUsers";
+import Form from "../pages/Form/Form";
+
+const AdminRoutes = [
+    {
+        path: '/',
+        component: Indicators,
+        layout: GeneralLayout
+    },
+    {
+        path: 'admin/users/list',
+        component: ListUsers,
+        layout: GeneralLayout
+    },
+    {
+        path: 'admin/users/create',
+        component: CreateUser,
+        layout: GeneralLayout
+    },
+    {
+        path: '/admin/form',
+        component: Form,
+        layout: GeneralLayout
+    }
+]
+
+const GeneralRoutes = [
+    {
+        path: '/form',
+        component: Form,
+        layout: GeneralLayout
+    }
+];
+
+const allRoutesProject = [...GeneralRoutes, ...AdminRoutes];
+
+export default allRoutesProject;
